295ccd643f569f9e292595114562fe1a9339476c
Verboom <dverboom@unica.nl>
2020-08-26 14:11:56 +0200

    update

--- [MM] - [NLDE] - MissionHelper.user.js
+++ [MM] - [NLDE] - MissionHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [MM] - [NLDE] - MissionHelper
 // @namespace    http://tampermonkey.net/
-// @version      2020.08.24.00.15
+// @version      2020.08.24.00.16
 // @description  try to take over the world!
 // @author       You
 // @updateRL     https://github.com/MoneyMalibu/MKS/raw/master/%5BMM%5D%20-%20%5BNLDE%5D%20-%20MissionHelper.user.js
@@ -10,7 +10,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     console.log("[MM] - [NLDE] - Missionhelper - START");
     Lightbox_Status();
 })();
@@ -22,13 +22,12 @@ function Lightbox_Status() {
         console.log("Status lightbox: " + window.getComputedStyle(x).display);
         //Openen van een nieuw meldingsscherm
         var newMissions = document.getElementById("mission_list").getElementsByClassName("panel panel-default  mission_panel_red");
-        console.log(newMissions);
         var teamMissions = document.getElementById("mission_list").getElementsByClassName("panel panel-default panel-success ");
-        console.log(teamMissions);
+        var teamsMissions = document.getElementById("mission_list_alliance").getElementsByClassName("panel panel-default panel-success");
+        console.log("[MM] - [NLDE] - Missionhelper - Data verkregen")
     }
 
-    setTimeout(function(){ 
-        Lightbox_Status(); 
+    setTimeout(function () {
+        Lightbox_Status();
     }, 1000);
-
 }
