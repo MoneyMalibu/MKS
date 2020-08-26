3cb6e233bb14f1f45191e38736d76d8c155a0ca0
Verboom <dverboom@unica.nl>
2020-08-26 14:30:13 +0200

    update

--- [MM] - [NLDE] - MissionHelper.user.js
+++ [MM] - [NLDE] - MissionHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [MM] - [NLDE] - MissionHelper
 // @namespace    http://tampermonkey.net/
-// @version      2020.08.24.00.16
+// @version      2020.08.24.00.17
 // @description  try to take over the world!
 // @author       You
 // @updateRL     https://github.com/MoneyMalibu/MKS/raw/master/%5BMM%5D%20-%20%5BNLDE%5D%20-%20MissionHelper.user.js
@@ -21,10 +21,14 @@ function Lightbox_Status() {
     if (window.getComputedStyle(x).display === "none") {
         console.log("Status lightbox: " + window.getComputedStyle(x).display);
         //Openen van een nieuw meldingsscherm
-        var newMissions = document.getElementById("mission_list").getElementsByClassName("panel panel-default  mission_panel_red");
-        var teamMissions = document.getElementById("mission_list").getElementsByClassName("panel panel-default panel-success ");
-        var teamsMissions = document.getElementById("mission_list_alliance").getElementsByClassName("panel panel-default panel-success");
+        var newMissions = document.getElementById("mission_list").getElementsByClassName("panel panel-default  mission_panel_red")[0];
+        //var teamMissions = document.getElementById("mission_list").getElementsByClassName("panel panel-default panel-success ");
+        //var teamsMissions = document.getElementById("mission_list_alliance").getElementsByClassName("panel panel-default panel-success");
         console.log("[MM] - [NLDE] - Missionhelper - Data verkregen")
+
+        //Missie openen
+        document.getElementById("mission_list").getElementsByClassName("panel panel-default  mission_panel_red")[0].children[0].childNodes[0].click;
+
     }
 
     setTimeout(function () {
