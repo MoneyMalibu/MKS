56469898939ef57d87f26d8c2ba221f886fab37c
Verboom <dverboom@unica.nl>
2020-08-26 13:48:41 +0200

    Update

--- [MM] - [NLDE] - MissionHelper.user.js
+++ [MM] - [NLDE] - MissionHelper.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         [MM] - [NLDE] - MissionHelper
 // @namespace    http://tampermonkey.net/
-// @version      2020.08.24.00.12
+// @version      2020.08.24.00.13
 // @description  try to take over the world!
 // @author       You
 // @updateRL     https://github.com/MoneyMalibu/MKS/raw/master/%5BMM%5D%20-%20%5BNLDE%5D%20-%20MissionHelper.user.js
 // @downloadURL  https://github.com/MoneyMalibu/MKS/raw/master/%5BMM%5D%20-%20%5BNLDE%5D%20-%20MissionHelper.user.js
-// @match        https://www.meldkamerspel.com/*
+// @match        https://www.meldkamerspel.com/
 // @grant        none
 // ==/UserScript==
 
@@ -19,7 +19,7 @@ function Lightbox_Status() {
     console.log("Status controle");
     var x = document.getElementById("lightbox_box");
     if (window.getComputedStyle(x).display === "none") {
-        console.log("Status lightbox: " & window.getComputedStyle(x).display)
+        console.log("Status lightbox: " + window.getComputedStyle(x).display);
     }
 
     setTimeout(function(){ Lightbox_Status; }, 1000);
