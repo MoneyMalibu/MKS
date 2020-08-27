11361dfe8c54ee648b67256a704dc1c9e007b976
MoneyMalibu <info@dverboom.nl>
2020-08-27 17:14:00 +0200

    update

--- [MM] - [NLDE] - MissionHelper.user.js
+++ [MM] - [NLDE] - MissionHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [MM] - [NLDE] - MissionHelper
 // @namespace    http://tampermonkey.net/
-// @version      2020.08.27.00.01
+// @version      2020.08.27.00.02
 // @description  try to take over the world!
 // @author       You
 // @updateRL     https://github.com/MoneyMalibu/MKS/raw/master/%5BMM%5D%20-%20%5BNLDE%5D%20-%20MissionHelper.user.js
